4d6b7f1cb749571e69ad1851bf9f911e728d61ac
Kyalo Josephine <145260844+jossyKyalo@users.noreply.github.com>
2023-11-12 20:24:43 +0300

    payroll system

--- MainFrame.java
+++ MainFrame.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 import java.awt.event.*;
 
 public class MainFrame{
-
+       //payroll details
 	private JFrame frame;
 	private JTextField employeeJina;
 	private JTextField rate;
